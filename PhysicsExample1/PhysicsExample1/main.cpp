72c22a32ad291a5b8000a0b360949b655bb86273
Bruno Berti <bsberti@gmail.com>
2022-09-09 23:33:12 -0400

    Vector3.h added

--- PhysicsExample1/PhysicsExample1/main.cpp
+++ PhysicsExample1/PhysicsExample1/main.cpp
@@ -1,93 +1,11 @@
 #include <iostream>
 
-float x;
-float y;
-float z;
-
-class Vector3 {
-	//private
-
-public:
-	//Default Constructor
-	Vector3();
-
-	//Copy Constructor
-	Vector3(const Vector3&);
-
-	//Assignment Overload
-	Vector3 operator=(const Vector3& rhs);
-
-	//Operator + Overload
-	Vector3& operator+(const Vector3& rhs);
-
-	//Operator - Overload
-	Vector3& operator-(const Vector3& rhs);
-
-	//Destructor
-	~Vector3();
-	
-	float m_X;
-	float m_Y;
-	float m_Z;
-};
+#include "Vector3.h"
 
 class Particle {
 public:
 	Vector3 position;
 };
-
-Vector3::Vector3() {
-	std::cout << " Vector3::Vector3()\n";
-	m_X = 0;
-	m_Y = 0;
-	m_Z = 0;
-}
-
-Vector3::Vector3(const Vector3& other)
-	: m_X(0)
-	, m_Y(0)
-	, m_Z(0)
-{
-	std::cout << " Vector3::Vector3(const Vector3& other)\n";
-}
-
-Vector3 Vector3::operator=(const Vector3& rhs) {
-	std::cout << " Vector3::operator=(const Vector3& rhs)\n";
-	m_X = rhs.m_X;
-	m_Y = rhs.m_Y;
-	m_Z = rhs.m_Z;
-	return *this;
-}
-
-Vector3& Vector3::operator+(const Vector3& rhs) {
-	std::cout << " Vector3::operator+(const Vector3& rhs)\n";
-	Vector3 newVector;
-	newVector.m_X = m_X + rhs.m_X;
-	newVector.m_Y = m_Y + rhs.m_Y;
-	newVector.m_Z = m_Y + rhs.m_Z;
-	return newVector;
-}
-
-Vector3& Vector3::operator-(const Vector3& rhs) {
-	std::cout << " Vector3::operator-(const Vector3& rhs)\n";
-	Vector3 newVector;
-	newVector.m_X = m_X + (-1 * rhs.m_X);
-	newVector.m_Y = m_Y + (-1 * rhs.m_Y);
-	newVector.m_Z = m_Y + (-1 * rhs.m_Z);
-	return newVector;
-}
-
-Vector3::~Vector3() {
-	std::cout << " Vector3::~Vector3()\n";
-}
-
-Vector3 Multiply(Vector3 v, float a) {
-	std::cout << " Multiply(Vector3 v, float a):\n";
-	v.m_X *= a;
-	v.m_Y *= a;
-	v.m_Z *= a;
-	return v;
-}
 
 void main()
 {
@@ -97,13 +15,13 @@ void main()
 	std::cout << "VectorB:\n";
 	Vector3 vB;
 
-	vA.m_X = 5;
-	vA.m_Y = 2;
-	vA.m_Z = 1;
+	vA.x = 5.f;
+	vA.y = 2.f;
+	vA.z = 1.f;
 
-	vB.m_X = 3;
-	vB.m_Y = 5;
-	vB.m_Z = 9;
+	vB.x = 3.f;
+	vB.y = 5.f;
+	vB.z = 9.f;
 
 	std::cout << "VectorA = VectorB:\n";
 	vA = vB;
@@ -122,12 +40,12 @@ void main()
 	std::cout << "VectorE:\n";
 	Vector3& vE = vD;
 
-	vD.m_X = 5;
-	vE.m_Y = 7;
+	vD.x = 5.f;
+	vE.y = 7.f;
 
 	std::cout << "VectorF:\n";
 	Vector3 vF;
-	vF = Multiply(vD, 3);
+	vF = vD * 3.f;
 
 	std::cout << "\n-------------------------------------------------------\n";
 
@@ -137,13 +55,13 @@ void main()
 	std::cout << "VectorH:\n";
 	Vector3 vH;
 
-	vG.m_X = 5;
-	vG.m_Y = 2;
-	vG.m_Z = 1;
+	vG.x = 5.0f;
+	vG.y = 2.0f;
+	vG.z = 1.0f;
 
-	vH.m_Y = 5;
-	vH.m_X = 3;
-	vH.m_Z = 9;
+	vH.x = 5.0f;
+	vH.y = 3.0f;
+	vH.z = 9.0f;
 
 	std::cout << "VectorI:\n";
 	Vector3 vI;
